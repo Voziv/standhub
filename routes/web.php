3b326947ca4c3613417108b7bbdce1b7dd168df8
Lee Robert <lrobert@leerobert.ca>
2018-12-21 10:46:41 -0500

    Add login protection. oops

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/auth/google/redirect', 'Auth\GoogleController@redirect')->name('google-login');
 Route::get('/auth/google/callback', 'Auth\GoogleController@callback');
 
-Route::get('/', 'WelcomeController@welcome');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/standup/send', 'StandupController@send')->name('standup-send');
-Route::get('/standup/thanks', 'StandupController@thanks')->name('standup-thanks');
+Route::get('/', 'WelcomeController@welcome')->name('login');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::post('/standup/send', 'StandupController@send')->name('standup-send');
+    Route::get('/standup/thanks', 'StandupController@thanks')->name('standup-thanks');
+});
